a70850937480ba7d75922ad13cb778cb70b49b35
onurhan1337 <onurhandtr@gmail.com>
2025-12-24 23:24:38 +0300

    feat: add complex access control rule to check kevi token

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,32 @@
 import { Hono } from "hono";
-import healthRoute from "./routes/health";
+import { cors } from "hono/cors";
+import kvRoute from "./routes/kv";
 
-const app = new Hono();
+/**
+ * Enterprise middleware patterns for future scaling:
+ * https://hono.dev/docs/middleware/builtin/combine#usage
+ * * Example:
+ * app.use('/v1/*', some(
+ * every(
+ * ipRestriction(getConnInfo, { allowList: ['192.168.0.2'] }),
+ * bearerAuth({ token })
+ * ),
+ * rateLimit()
+ * ))
+ */
 
-app.route("/v1", healthRoute);
+const app = new Hono<{ Bindings: Env }>();
+
+app.use("*", async (c, next) => {
+  const apiToken = c.req.header("X-Kevi-Token");
+
+  if (c.env.API_TOKEN && apiToken !== c.env.API_TOKEN) {
+    return c.json({ error: "Forbidden" }, 403);
+  }
+
+  return await cors()(c, next);
+});
+
+app.route("/v1/:apiKey", kvRoute);
 
 export default app;
